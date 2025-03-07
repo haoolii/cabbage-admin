c59e3dcbecf7fd780991c3f9176863ed3fb81376
haoolii <unnhao@gmail.com>
2025-03-07 15:40:37 +0800

    feat: test deploy

---
+++ .open-next/env/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NODE_ENV":"development","APP_NAME":"Hello","CLIENT_BASE":"http://localhost:3000","CLIENT_API_BASE":"http://localhost:3000","SERVER_API_BASE":"http://localhost:4321"};
+export const development = {"NODE_ENV":"development","APP_NAME":"Hello","CLIENT_BASE":"http://localhost:3000","CLIENT_API_BASE":"http://localhost:3000","SERVER_API_BASE":"http://localhost:4321"};
+export const test = {"NODE_ENV":"development","APP_NAME":"Hello","CLIENT_BASE":"http://localhost:3000","CLIENT_API_BASE":"http://localhost:3000","SERVER_API_BASE":"http://localhost:4321"};
